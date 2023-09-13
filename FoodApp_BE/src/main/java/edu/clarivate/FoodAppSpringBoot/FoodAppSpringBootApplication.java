f25442128fbe46dce40878b59871d46bbdecd3b6
Pavan M <pavanmanjunath768@gmail.com>
2023-09-13 18:57:09 +0530

    first commit

---
+++ FoodApp_BE/src/main/java/edu/clarivate/FoodAppSpringBoot/FoodAppSpringBootApplication.java
@@ -0,0 +1,13 @@
+package edu.clarivate.FoodAppSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodAppSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodAppSpringBootApplication.class, args);
+	}
+
+}
---
+++ FoodApp_BE/src/test/java/edu/clarivate/FoodAppSpringBoot/FoodAppSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.clarivate.FoodAppSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodAppSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
